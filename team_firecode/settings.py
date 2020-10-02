9bc00ca6bdbaf8b9192a54d71f77ad129364de2b
mohitsuthar08 <mohitsuthar08@gmail.com>
2020-10-02 09:30:27 +0530

    css html commit

--- team_firecode/settings.py
+++ team_firecode/settings.py
@@ -77,11 +77,26 @@ WSGI_APPLICATION = 'team_firecode.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+"""
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'team_firecode',
+'USER': 'root',
+'PASSWORD':'',
+'HOST': '127.0.0.1',
+'PORT':'3306',
+'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+}
+}
 }
 
 
