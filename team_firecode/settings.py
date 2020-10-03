fadad317cf1e11b3b11a8912f0f0949794f5ad5f
mohitsuthar08 <mohitsuthar08@gmail.com>
2020-10-03 07:56:09 +0530

    server commit

--- team_firecode/settings.py
+++ team_firecode/settings.py
@@ -77,7 +77,6 @@ WSGI_APPLICATION = 'team_firecode.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -98,7 +97,7 @@ DATABASES = {
 }
 }
 }
-
+"""
 
 
 # Password validation
